43742af04b9e2700a495f045703d4e0e27246bd0
Imad <imadouairem96@gmail.com>
2023-12-17 00:38:50 +0100

    Implement Circuit Breaker

--- account-service/src/main/java/ma/ouairem/accountservice/clients/CustomerRestClient.java
+++ account-service/src/main/java/ma/ouairem/accountservice/clients/CustomerRestClient.java
@@ -1,5 +1,6 @@
 package ma.ouairem.accountservice.clients;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import ma.ouairem.accountservice.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,22 @@ import java.util.List;
 @FeignClient(name="CUSTOMER-SERVICE")
 public interface CustomerRestClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerService", fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable  Long id);
     @GetMapping("/customers")
+    @CircuitBreaker(name="customerService", fallbackMethod = "getAllCustomers")
     List<Customer> allCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName("Not Available");
+        customer.setLastName("Not Available");
+        customer.setEmail("Not Available");
+        return customer;
+    }
+
+    default List<Customer> getAllCustomers(Exception exception) {
+        return List.of();
+    }
 }
